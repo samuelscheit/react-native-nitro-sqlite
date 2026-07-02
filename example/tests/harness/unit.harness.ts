a4fb941283cf504d46059e136fb90f5359cdb8d7
Christoph Pader <chris@margelo.com>
2026-07-02 12:30:18 +0200

    fix: init quickcrypto in harness tests

--- example/tests/harness/unit.harness.ts
+++ example/tests/harness/unit.harness.ts
@@ -1,6 +1,6 @@
-import * as harnessTestApi from 'react-native-harness'
+import '@/initGlobals'
 import { registerUnitTests } from '@tests/unit'
-import { setTestApi } from '@tests/TestApi'
+import init from './init'
 
-setTestApi(harnessTestApi)
+init()
 registerUnitTests()
