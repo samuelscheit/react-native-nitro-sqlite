a904af731bf9a934e9cc5c4352528f82352dde1f
Christoph Pader <chris@margelo.com>
2026-07-02 12:28:33 +0200

    chore: add babel polyfills for RNQuickCrypto

--- example/babel.config.js
+++ example/babel.config.js
@@ -10,7 +10,9 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js', '.json'],
         alias: {
           [pak.name]: path.join(__dirname, '../package', pak.source),
-          'stream': 'stream-browserify',
+          'crypto': 'react-native-quick-crypto',
+          'stream': 'readable-stream',
+          'buffer': 'react-native-quick-crypto',
           'react-native-sqlite-storage': 'react-native-nitro-sqlite',
           '^@/(.+)': './src/\\1',
           '^@tests/(.+)': './tests/\\1',
